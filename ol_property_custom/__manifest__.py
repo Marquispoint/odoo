65894bcfa203044d6f6cb821410fa9623b7c6c1e
Marquispoint <112481434+Marquispoint@users.noreply.github.com>
2022-11-16 00:13:58 +0500

    Add files via upload

---
+++ ol_property_custom/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    "name": "Property",
+
+    "author": "Huzaifa",
+    "category": "sale",
+
+    "license": "OPL-1",
+
+    "version": "15.0.1",
+
+    "depends": [
+        'project','sale_management', 'contacts', 'product', 'crm', 'ol_sales_agreement_report', 'account',
+    ],
+
+    "data": [
+        'security/ir.model.access.csv',
+        'wizard/create_building.xml',
+        'views/so_inherit.xml',
+        'views/main_view.xml',
+        'views/fields.xml',
+        # 'views/installment_invoice_button.xml',
+
+    ],
+    
+    "images": [ ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+}
