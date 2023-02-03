83f44570ce6bd65e4614d0fd62822e462dd4afee
Muhammad Rizwan <rizwangcsit@gmail.com>
2023-02-03 17:26:43 +0500

    issue resolved

--- pdc_payments/__manifest__.py
+++ pdc_payments/__manifest__.py
@@ -9,8 +9,8 @@
         PDC Payments Cheque Print
     """,
 
-    'author': "Demo",
-    'website': "http://www.demo.com",
+    'author': "Viltco",
+    'website': "http://www.viltco.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
