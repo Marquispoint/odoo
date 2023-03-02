38ded76bca9fc85c72afe0928e2e8c04eb427e60
Muhammad Rizwan <58412444+rizwanch786@users.noreply.github.com>
2023-03-02 19:29:20 +0500

    final

--- ol_sales_agreement_report/models/main_view.py
+++ ol_sales_agreement_report/models/main_view.py
@@ -11,17 +11,17 @@ class ResPartnerInherited(models.Model):
     _inherit = 'res.partner'
 
     name_arabic = fields.Char(string='Name Arabic')
-    is_unit = fields.Boolean('Is Unit')
-
-
-class CrmLead(models.Model):
-    _inherit = 'crm.lead'
-
-    partner_id = fields.Many2one(comodel_name='res.partner', domain='[("is_unit", "=", False)]')
-
-
-class PurchaserCompanyInherit(models.Model):
-    _inherit = 'purchaser.company'
-
-    purchase_individual = fields.Many2one(comodel_name='res.partner', string='Individual',
-                                          domain='[("is_unit", "=", False)]')
+#     is_unit = fields.Boolean('Is Unit')
+#
+#
+# class CrmLead(models.Model):
+#     _inherit = 'crm.lead'
+#
+#     partner_id = fields.Many2one(comodel_name='res.partner', domain='[("is_unit", "=", False)]')
+#
+#
+# class PurchaserCompanyInherit(models.Model):
+#     _inherit = 'purchaser.company'
+#
+#     purchase_individual = fields.Many2one(comodel_name='res.partner', string='Individual',
+#                                           domain='[("is_unit", "=", False)]')
