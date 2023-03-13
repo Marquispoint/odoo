ea1ff3f96af72959296b82a7a823f1ac2c2370e3
Muhammad Rizwan <58412444+rizwanch786@users.noreply.github.com>
2023-03-13 14:00:35 +0500

    LPO, Interim and TRN

--- branch/models/branch.py
+++ branch/models/branch.py
@@ -8,6 +8,7 @@ class ResBranch(models.Model):
     _description = 'Branch'
 
     name = fields.Char(required=True)
+    vat = fields.Char(string='TRN')
     company_id = fields.Many2one('res.company', required=True)
     telephone = fields.Char(string='Telephone No')
     address = fields.Text('Address')
--- branch/models/inherited_purchase_order.py
+++ branch/models/inherited_purchase_order.py
@@ -28,11 +28,7 @@ class PurchaseOrder(models.Model):
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    def _default_branch_id(self):
-        branch_id = self.env['res.users'].browse(self._uid).branch_id.id
-        return branch_id
-
-    branch_id = fields.Many2one('res.branch', default=_default_branch_id)
+    branch_id = fields.Many2one('res.branch')
 
     @api.onchange('branch_id')
     def _onchange_branch_id_tag_change(self):
