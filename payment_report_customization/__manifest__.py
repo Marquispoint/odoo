e4616898c0b3f6921978c04e7877c78104245634
abdullahk578 <143720318+abdullahk578@users.noreply.github.com>
2023-11-08 16:26:23 +0500

    Payment Report Updated

--- payment_report_customization/__manifest__.py
+++ payment_report_customization/__manifest__.py
@@ -8,7 +8,7 @@
     'version': '15.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account'],
+    'depends': ['base', 'account','web'],
 
     # always loaded
     'data': [
--- payment_report_customization/models/models.py
+++ payment_report_customization/models/models.py
@@ -5,9 +5,20 @@ from num2words import num2words
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
+    from num2words import num2words
+
     @api.depends('amount', 'currency_id')
     def compute_text(self):
-        return num2words(self.amount_total).upper()
+        for record in self:
+            amount_without_commas = str(record.amount).replace(',', '')  # Remove commas from the amount
+            text = num2words(amount_without_commas, lang='en')
+            text_without_commas = text.replace(',', '')  # Remove commas from the words
+            abc = text_without_commas.upper()
+            return abc
+
+    # @api.depends('amount', 'currency_id')
+    # def compute_text(self):
+    #     return num2words(self.amount_total).upper()
 
     def get_dynamic_header(self):
         for rec in self:
@@ -21,5 +32,20 @@ class AccountPayment(models.Model):
                 return '<h4>Bank Payment Voucher</h4>'
             else:
                 return '<h4></h4>'
+
+    def customer_supplier(self):
+        for rec in self:
+            # if rec.payment_type == 'inbound' and rec.journal_id.type == 'cash':
+            #     return '<strong>Customer</strong>'
+            # elif rec.payment_type == 'inbound' and rec.journal_id.type == 'bank':
+            #     return '<strong>Customer</strong>'
+            # elif rec.payment_type == 'outbound' and rec.journal_id.type == 'cash':
+            #     return '<strong>Customer</strong>'
+            if rec.payment_type == 'outbound' and rec.journal_id.type == 'bank':
+                return '<strong>Supplier:</strong>'
+            else:
+                return '<strong>Customer:</strong>'
+
+
 
 
